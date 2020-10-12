ce2ae0554579c789202b6bac4a0ff423c706502e
kaixu <kaixumvp@163.com>
2020-10-12 23:36:23 +0800

    新加仓库系统

---
+++ storemark/src/main/java/com/heiiyo/storemark/StoremarkApplication.java
@@ -0,0 +1,13 @@
+package com.heiiyo.storemark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoremarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoremarkApplication.class, args);
+	}
+
+}
---
+++ storemark/src/test/java/com/heiiyo/storemark/StoremarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heiiyo.storemark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoremarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
